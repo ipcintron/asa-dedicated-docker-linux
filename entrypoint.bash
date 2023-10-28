5a62b7559ebb9c3e889cf30a4e1b43ee2feca51b
ipcintron <ipcintron1@gmail.com>
2023-10-27 21:16:29 -0500

    changed app ID

--- entrypoint.bash
+++ entrypoint.bash
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-runuser -l wine bash -c 'steamcmd +login anonymous +@sSteamCmdForcePlatformType windows +force_install_dir /appdata/asa/asa +app_update 298740 +quit'
+runuser -l wine bash -c 'steamcmd +login anonymous +@sSteamCmdForcePlatformType windows +force_install_dir /appdata/asa/asa +app_update 2430930 +quit'
 runuser -l wine bash -c '/entrypoint-asa.bash'
